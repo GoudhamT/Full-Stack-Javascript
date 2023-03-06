2e498eb557c34c1f9a32b4a04b6ed99dc24d8f8e
Goudham Thangavelu <goudham@MADIBAINDIASERVER.COM>
2023-03-06 23:09:02 +0530

    my first JS

---
+++ Javascript/my_first.js
@@ -0,0 +1,11 @@
+let firstname = "Goudham"
+let lastname = 'T'
+let isabove40 = false
+const gender = 'Male'
+const mobilno = 9876543210
+
+console.log('My first name is ',firstname)
+console.log("my last name is"+lastname)
+console.log(`other detail are ${isabove40},${gender},${mobilno}`)
+
+console.log('my details are'+isabove40,gender,`${mobilno}`)
