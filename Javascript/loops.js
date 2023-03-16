b5e9f23672849a0cfc9ceeb034144db8987e59b5
goudham <goudhamvel.88@gmail.com>
2023-03-16 21:50:18 +0530

    loops in JS

---
+++ Javascript/loops.js
@@ -0,0 +1,19 @@
+//Loops in JS
+for(i=0;i<=5;i++)
+{
+    console.log(`${i}*${i} is ${i*i}`)
+}
+
+//do while -> loop is executed atleast once before checking condition
+j = 0
+do
+{
+    console.log(j)
+    j++
+}while(j<6)
+
+k=0
+while(k<10){
+    console.log(k)
+    ++k;
+}
