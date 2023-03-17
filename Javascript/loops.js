d691fc55839b99fd0174d4c038001f174e165423
goudham <goudhamvel.88@gmail.com>
2023-03-17 20:04:20 +0530

    for of loop

--- Javascript/loops.js
+++ Javascript/loops.js
@@ -17,3 +17,20 @@ while(k<10){
     console.log(k)
     ++k;
 }
+
+//for of loop
+myarr = ["goudham","niralya","nithila",1,2,true]
+myarr_upper = []
+
+for(let name of myarr)
+{
+    if(typeof(name)=== 'string'){
+        myarr_upper.push(name.toUpperCase())
+    }
+    else
+    {
+        continue
+    }
+    
+}
+console.log(myarr_upper)
